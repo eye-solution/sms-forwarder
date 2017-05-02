53847af82c61e1a42726054d48dc37ba11c86fdc
Thien Nguyen <thiennq@Thiens-MacBook-Pro.local>
2017-05-02 13:08:33 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# SMS Forwarder
+
+## What's this app?
+  This app is used for listening your SMSs and forwarding to a server.
+
+## How to use
+  Just run it, and keep it running in background   
+  (make sure that you don't kill it).
+
+## How to use modify it?
+  Take a look at the <code>src/app/app.component.ts</code> file,  
+  update <code>apiEndpoint</code> variable for your api end-point.
+
+## Disclaimer
+  This app need permission to access your SMS which can violate the privacy of users.  
+  Please consider for using in commerce.
+
+## Build Instruction
+
+- Install Android Studio: https://developer.android.com/studio/install.html
+- Install cordova & ionic
+```bash
+#install cordova
+npm install -g cordova
+#install ionic
+npm install -g ionic
+```
+
+- Clone project & install dependencies
+```bash
+git clone https://github.com/eye-solution/sms
+cd sms
+npm install
+```
+- Generate app icon and splash screen
+```bash
+# replace resources/icon.png with your icon
+# replace resources/splash.png with your splash screen
+ionic resources # generate multi sizes for icon/splash
+```
---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import { HomePage } from '../pages/home/home';
+declare var SMS:any;
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = HomePage;
+  apiEndpoint:string = 'http://eyeteam.vn:3000/test';
+
+  constructor(public http: Http, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    let $http = this.http;
+    let $endPoint = this.apiEndpoint;
+
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+
+      if (SMS) {
+        SMS.startWatch(function(){
+              console.log('watching started');
+           }, function(){
+          console.log('failed to start watching');
+        });
+        document.addEventListener('onSMSArrive', function(e:any){
+          var sms = (e && e.data) || false;
+          console.log(sms);
+
+          $http.post($endPoint, sms, {})
+            .toPromise()
+            .then((res:Response) => {
+              let data = res.json();
+              console.log(data);
+            })
+            .catch(error => {
+              console.log('cannot send request');
+              console.log(error.error); // error message as string
+              console.log(error.status, error.headers);
+            });
+
+        });
+      } else {
+        console.log('SMS is ', SMS);
+      }
+    });
+  }
+}
